23db3d57dfcda46481d1be57259f4e42a221ed74
Sajeed <syedsajeed6969@gmail.com>
2023-12-10 08:55:11 +0530

    footer updated

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,11 +1,12 @@
-import { Row, Col } from "react-bootstrap";
-import '../App.css';
+import { Row, Col } from "react-bootstrap"
+import '../App.css'
 const Footer = () => {
   return (
     <>
       <footer>
           <Row>
-            <Col className="text-center"> <div className="mt-3"> Copyright &copy; 2023</div>
+            <Col className="text-center"> 
+            <div className="mt-3"> Copyright &copy; 2023</div>
             </Col>
           </Row>
       </footer>
