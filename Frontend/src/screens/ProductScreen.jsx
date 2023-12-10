7c3c415344d1fc51c6e66902b143a4a1597d8e0c
Sajeed <syedsajeed6969@gmail.com>
2023-12-10 09:04:31 +0530

    productscreen link added

--- Frontend/src/screens/ProductScreen.jsx
+++ Frontend/src/screens/ProductScreen.jsx
@@ -3,16 +3,19 @@ const ProductScreen = ({ product }) => {
     return (
         <>
             <Card style={{ width: '18rem' }} className="m-2">
-                <Card.Img variant="top" src={product.image} />
+                <a href={`/product/${product._id}`}>
+                    <Card.Img variant="top" src={product.image} />
+                </a>
                 <Card.Body>
-                    <Card.Title>{product.name}</Card.Title>
-                    <Card.Text>
-                        {product.description}
-                    </Card.Text >
-                    <div className="text-center"> 
-                    <Button variant="primary">Go somewhere</Button>
-                    </div>
-                   
+                    <a href={`/product/${product._id}`}>
+                        <Card.Title>{product.name}</Card.Title>
+                        <Card.Text>
+                            {product.rating} from {product.numReviews}
+                        </Card.Text >
+                        <div className="text-center">
+                            <Button variant="primary">Go somewhere</Button>
+                        </div>
+                    </a>
                 </Card.Body>
             </Card>
         </>
