7ed7010e09914fc663673a320c91abda641903fb
Sajeed Syed <123931227+mrsajids@users.noreply.github.com>
2023-12-12 22:22:26 +0530

    Update Header.jsx

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav';
+import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 const Header = () => {
     return (
