424e0bf9747bb4a5aa9f46cf1bd74fef3afe29db
Sajeed <syedsajeed6969@gmail.com>
2023-12-15 09:05:07 +0530

    info.txt updated

--- Frontend/src/screens/ProductDetails.jsx
+++ Frontend/src/screens/ProductDetails.jsx
@@ -1,15 +1,15 @@
-import { Col, ListGroup, ListGroupItem, Row,Button } from "react-bootstrap";
+import { Col, ListGroup, ListGroupItem, Row, Button } from "react-bootstrap";
 import Products from "../products"
 import { Link, useParams } from 'react-router-dom';
 import Rating from "./Rating";
-const ProductDetails = ({ match }) => {
+const ProductDetails = () => {
     const { id } = useParams();
     // const products = Products.find((p)=>p._id===match.params.id) uska wala
     const product = Products.find((p) => p._id === id)
     return (
         <>
-        <Link to="/" className="btn btn-light">
-           <i className="fas fa-arrow-left"></i> Go Back</Link>
+            <Link to="/" className="btn btn-light">
+                <i className="fas fa-arrow-left"></i> Go Back</Link>
             <Row>
                 <Col md={6}>
                     <img src={product.image} alt={product.name} fluid />
@@ -26,21 +26,21 @@ const ProductDetails = ({ match }) => {
                                 text={`${product.numReviews} Reviews`} />
                         </ListGroupItem>
                         <ListGroupItem>Price : ${product.price}</ListGroupItem>
-            <ListGroupItem>{product.description}</ListGroupItem>
+                        <ListGroupItem>{product.description}</ListGroupItem>
                     </ListGroup>
                 </Col>
                 <Col>
-                <ListGroupItem>
-                    <Row>
-        <Col> Status:</Col>
-                <Col>{product.countInStock>0?'in stock':'out of stock'}</Col>
-                </Row>
-                </ListGroupItem>
-                   <ListGroupItem>
-                    <Button>add to cart</Button>
-                   </ListGroupItem>
+                    <ListGroupItem>
+                        <Row>
+                            <Col> Status:</Col>
+                            <Col>{product.countInStock > 0 ? 'in stock' : 'out of stock'}</Col>
+                        </Row>
+                    </ListGroupItem>
+                    <ListGroupItem>
+                        <Button>add to cart</Button>
+                    </ListGroupItem>
                 </Col>
-                
+
             </Row>
         </>
     )
