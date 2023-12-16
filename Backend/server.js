9e961c2ab78b074ed6ac3fe92d5d3dd4aba53b43
Sajeed <syedsajeed6969@gmail.com>
2023-12-16 09:01:20 +0530

    products.json updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app=express()
+const products= require('./data/products')
 
 app.get("/",(req,res)=>{
     res.send('<h1>app working properly..</h1>')
+})
+app.get("/products",(req,res)=>{
+    res.json(products)
+})
+app.get("/products/:id",(req,res)=>{
+    // const product=products.find((p)=>res.send(p._id))
+    // res.json(req.params.id)
 })
 
 app.listen(4000,()=>{
