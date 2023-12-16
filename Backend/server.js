351eb9a1f9c1336dbe2285f270da8f00f42652e3
Sajeed <syedsajeed6969@gmail.com>
2023-12-16 15:25:42 +0530

    backend code formated

--- Backend/server.js
+++ Backend/server.js
@@ -1,22 +1,22 @@
 const express = require('express')
 const cors = require('cors')
-const products= require('./data/products')
-const app=express()
+const products = require('./data/products')
+const app = express()
 
 //cors error
 app.use(cors())
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send('<h1>app working properly..</h1>')
 })
-app.get("/products",(req,res)=>{
+app.get("/products", (req, res) => {
     res.json(products)
 })
-app.get("/products/:id",(req,res)=>{
-    const product=products.find((p)=>p._id===req.params.id)
+app.get("/products/:id", (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
     res.json(product)
 })
 
-app.listen(4000,()=>{
-    console.log('Server running on',4000);
+app.listen(4000, () => {
+    console.log('Server running on', 4000);
 })
