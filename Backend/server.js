1cd940d6e105a37ebd1d05542a525621ac8d1d8a
Sajeed <syedsajeed6969@gmail.com>
2023-12-17 14:28:13 +0530

    server updated

--- Backend/server.js
+++ Backend/server.js
@@ -13,9 +13,11 @@ dotenv.config()
 app.get("/", (req, res) => {
     res.send('<h1>app working properly..</h1>')
 })
+
 app.get("/products", (req, res) => {
     res.json(products)
 })
+
 app.get("/products/:id", (req, res) => {
     const product = products.find((p) => p._id === req.params.id)
     res.json(product)
