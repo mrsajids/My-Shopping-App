fb27316eeb86248d97e09691bda7598f059c1385
Sajeed <syedsajeed6969@gmail.com>
2023-12-18 09:47:47 +0530

    model created and data imported

--- Backend/data/products.js
+++ Backend/data/products.js
@@ -1,6 +1,6 @@
 const products = [
   {
-    _id: '1',
+    
     name: 'boAt Airdopes 121v2 TWS Earbuds',
     image: '/images/boatHeadfone.jpg',
     description:
@@ -13,7 +13,7 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: '2',
+   
     name: 'Micromax IN 1b (Purple, 32 GB)',
     image: '/images/micromaxInB.jpg',
     description:
@@ -26,7 +26,7 @@ const products = [
     numReviews: 8,
   },
   {
-    _id: '3',
+   
     name: 'Cannon EOS 80D DSLR Camera',
     image: '/images/camera.jpg',
     description:
@@ -39,7 +39,7 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: '4',
+    
     name: 'Sony Playstation 4 Pro White Version',
     image: '/images/playstation.jpg',
     description:
@@ -52,7 +52,7 @@ const products = [
     numReviews: 12,
   },
   {
-    _id: '5',
+    
     name: 'Logitech G-Series Gaming Mouse',
     image: '/images/mouse.jpg',
     description:
@@ -65,7 +65,7 @@ const products = [
     numReviews: 10,
   },
   {
-    _id: '6',
+   
     name: 'Amazon Echo Dot 3rd Generation',
     image: '/images/alexa.jpg',
     description:
---
+++ Backend/seeder.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const users = require("./data/user");
+const Order = require("./moldel/orderModel");
+const Product = require("./moldel/productModel");
+const User = require("./moldel/userModel");
+
+const products = require("./data/products");
+const dbconnection = require("./config/config");
+
+dotenv.config();
+dbconnection();
+
+const importData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+    const createUser = await User.insertMany(users);
+    const adminUser = createUser[0]._id;
+    const sampleData = products.map((product) => {
+      return { ...product, user: adminUser };
+    });
+    await Product.insertMany(sampleData);
+    console.log("Data Imported!!");
+    process.exit();
+  } catch (error) {
+    console.log(`${error}`);
+    process.exit(1);
+  }
+};
+
+const dataDestory = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+    console.log("Data Destory");
+    process.exit();
+  } catch (error) {
+    console.log(`${error}`);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-d") {
+  dataDestory();
+} else {
+  importData();
+}
