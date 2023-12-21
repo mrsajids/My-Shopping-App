bfbf94559e3e11e2e2341d32c4dd5b3c0c480556
Sajeed <syedsajeed6969@gmail.com>
2023-12-21 16:32:21 +0530

    validate dom fixed

--- Frontend/src/screens/ProductScreen.jsx
+++ Frontend/src/screens/ProductScreen.jsx
@@ -1,6 +1,8 @@
 import { Card } from "react-bootstrap"
 import Rating from "./Rating"
 import { Link } from "react-router-dom"
+import { LinkContainer } from 'react-router-bootstrap'
+
 const ProductScreen = ({ product }) => {
     return (
         <>
@@ -9,15 +11,16 @@ const ProductScreen = ({ product }) => {
                     <Card.Img variant="top" src={product.image} />
                 </Link>
                 <Card.Body className="text-center">
-                    <Link to={`/product/${product._id}`}>
+                    <LinkContainer to={`/product/${product._id}`}>
                         <Card.Title>{product.name}</Card.Title>
+                        </LinkContainer>   
                         <Card.Text>
                             <Rating  value={product.rating} text={`${product.numReviews} reviews`} />
                         </Card.Text>
                         <div>
                         &#x24; {product.price}
                         </div>
-                    </Link>               
+                                
                 </Card.Body>
             </Card>
         </>
