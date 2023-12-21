ec4f1fe11c543b29e6f78603bb1d02b4301e2787
Sajeed <syedsajeed6969@gmail.com>
2023-12-21 11:43:11 +0530

    constant added

--- Frontend/src/action/productAction.js
+++ Frontend/src/action/productAction.js
@@ -1,20 +1,25 @@
 import axios from 'axios'
+import {
+    PRODUCT_LIST_REQUEST,
+    PRODUCT_LIST_SUCCESS,
+    PRODUCT_LIST_FAIL,
+} from '../constants/constants'
 
 export const listProduct = async (dispatch) => {
     try {
         dispatch({
-            type: 'PRODUCT_LIST_REQUEST'
+            type: PRODUCT_LIST_REQUEST
         })
         const { data } = await axios.get('/products')
         dispatch({
-            type: 'PRODUCT_LIST_SUCCESS',
+            type: PRODUCT_LIST_SUCCESS,
             payload: data
         })
     } catch (error) {
         dispatch({
-            type: 'PRODUCT_LIST_FAIL',
-            payload: error.response && error.response.data.message ? 
-            error.response.data.message : error.message
+            type: PRODUCT_LIST_FAIL,
+            payload: error.response && error.response.data.message ?
+                error.response.data.message : error.message
         })
     }
 
