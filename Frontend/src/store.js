4602034a342d35a68f65d5d1ff0f07d012a8613f
Sajeed <syedsajeed6969@gmail.com>
2023-12-21 07:38:41 +0530

    dev tool error fixed

--- Frontend/src/store.js
+++ Frontend/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware,compose } from 'redux'
 import { thunk } from 'redux-thunk'
 import { productListReducer } from './reducer/productReducer'
 // import { composeWithDevTools } from 'redux-devtools-extension'
@@ -8,10 +8,11 @@ const reducer = combineReducers({
     productList: productListReducer
 })
 const middleware = [thunk]
+const composeEnhancer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const store = createStore(
     reducer,
-    initialState, applyMiddleware(...middleware)
+    initialState, composeEnhancer(applyMiddleware(...middleware))
 )
 
 export default store
