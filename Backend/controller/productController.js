79081863ab47154c71a3fd52d640d536bec1924a
Sajeed <syedsajeed6969@gmail.com>
2023-12-23 21:24:01 +0530

    controller added

--- Backend/controller/productController.js
+++ Backend/controller/productController.js
@@ -1,8 +1,21 @@
-const Product = require('../moldel/userModel')
+const Product = require('../moldel/productModel')
 
 const getProducts = async (req, res) => {
     const products = await Product.find({})
     res.json(products)
 }
 
-module.exports = { getProducts }
+const getProduct = async (req, res)=>{
+    const product = await Product.findById(req.params.id)
+    try {
+        if (product) {
+        res.json(product)
+    } else {
+        res.status(404).json({ message: "product not found" })
+    }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = { getProducts,getProduct }
--- Backend/router/productRoute.js
+++ Backend/router/productRoute.js
@@ -1,29 +1,31 @@
 const express = require('express')
 const router = express.Router()
-const Product = require('../moldel/productModel')
-const {getProducts} = require('../controller/productController')
+const {getProducts,getProduct} = require('../controller/productController')
 
 router.get('/products/', getProducts)
 
-//get all products
+
+router.get('/products/:id', getProduct)
+
+// get all products
 // router.get('/products/', async (req, res) => {
 //     const product = await Product.find({})
 //     res.json(product)
 // })
 
 //get single product
-router.get('/products/:id', async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    try {
-        if (product) {
-        res.json(product)
-    } else {
-        res.status(404).json({ message: "product not found" })
-    }
-    } catch (error) {
+// router.get('/products/:id', async (req, res) => {
+//     const product = await Product.findById(req.params.id)
+//     try {
+//         if (product) {
+//         res.json(product)
+//     } else {
+//         res.status(404).json({ message: "product not found" })
+//     }
+//     } catch (error) {
         
-    }
+//     }
     
-})
+// })
 
 module.exports = router
