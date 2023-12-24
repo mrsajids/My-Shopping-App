65813922f20bf3a61e0256db9b1d53b2d34e5fbb
Sajeed <syedsajeed6969@gmail.com>
2023-12-24 08:39:54 +0530

    error solved

--- Backend/router/userRoute.js
+++ Backend/router/userRoute.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const {authController} =require('../controller/userController')
+const { authController } = require('../controller/userController')
 
-router.post('/login',authController)
+router.post('/login', authController)
 
 module.exports = router
