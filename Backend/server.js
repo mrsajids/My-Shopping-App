1b4cf00fa98485ea3b51e99916a097596e936744
Sajeed <syedsajeed6969@gmail.com>
2024-01-12 22:30:57 +0530

    Code Complete

--- Backend/server.js
+++ Backend/server.js
@@ -36,18 +36,6 @@ app.get("/api/config/paypal", (req, res) => {
 });
 
 app.use(errorHandler)
-// app.get("/", (req, res) => {
-//     res.send('<h1>Welcome to the server..</h1>')
-// })
-
-// app.get("/products", (req, res) => {
-//     res.json(products)
-// })
-
-// app.get("/products/:id", (req, res) => {
-//     const product = products.find((p) => p._id === req.params.id)
-//     res.json(product)
-// })
 
 const PORT = 4000
 app.listen(process.env.PORT || PORT, () => {
